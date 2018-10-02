3799878678b59d4d6275d535c19802db5154bb36
Tomas Tomov <tomastomov@yahoo.com>
2018-10-03 00:16:53 +0300

    Test commit

--- NewsService.Web/Controllers/HomeController.cs
+++ NewsService.Web/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace NewsService.Web.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your News description page.";
 
             return View();
         }
